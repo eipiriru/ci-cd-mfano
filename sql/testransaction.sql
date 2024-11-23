c0c05caa7ba236a1cf58df8236982b72f9bdb84f
eipiriru <ainurkhaprianto@gmail.com>
2024-11-23 11:57:01 +0700

    tes tr

--- sql/testransaction.sql
+++ sql/testransaction.sql
@@ -1,2 +1,2 @@
-INSERT INTO random(nama,alamat,jenis_kelamin) VALUES ('Lewbron','Soundvillage','Laki-laki');
-INSERT INTO random(nama,alamat,jenis_kelamin) VALUES ('Merlin','Soundvillage','Laki-laki');
+INSERT INTO random(nama,alamat,jenis_kelamin) VALUES ('Nen','Soundvillage','Laki-laki');
+INSERT INTO random(nama,alamat,jenis_kelaminm) VALUES ('Op','Soundvillage','Laki-laki');
